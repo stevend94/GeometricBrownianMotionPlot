5ddd8aa9a3b9991f712285e6a5fe770c2860d848
Stevend94 <srderby94@gmail.com>
2016-09-11 21:47:31 +0100

    updated Main.java template again, this time changed default drift rates

--- Main.java
+++ Main.java
@@ -10,18 +10,18 @@ public class Main {
      * @param args the command line arguments
      */
 
-  private final static int ITERATIONS = 400;
-  private final static int PROCESSES = 5;
+  private final static int ITERATIONS = 100;
+  private final static int PROCESSES = 4;
   private final static float INCREMENT = 0.01f;
 
   public static void main(String[] args) {
       // TODO code application logic here
       GeometricBrownianMotion gbm = new GeometricBrownianMotion(PROCESSES, ITERATIONS, INCREMENT);
-      gbm.setProcess(0, 5, 0.01f, 0.5f);
-      gbm.setProcess(1, 5, 0.01f, 0.4f, true);
-      gbm.setProcess(2, 5, 0.01f, 0.3f);
-      gbm.setProcess(3, 5, 0.01f, 0.2f);
-      gbm.setProcess(4, 5, 0.01f, 0.1f);
+      gbm.setProcess(0, 1, 0.1f, 0.4f);
+      gbm.setProcess(1, 1, 0.1f, 0.3f, true);
+      gbm.setProcess(2, 1, 0.2f, 0.2f);
+      gbm.setProcess(3, 1, 0.1f, 0.1f);
+      //gbm.setProcess(4, 5, 0.01f, 0.2f, true);
       DataGrapher graph = new DataGrapher(gbm.generateData());
 
       JFrame f = new JFrame();
